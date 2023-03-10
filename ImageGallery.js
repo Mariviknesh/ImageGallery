13e7b5d076165f82b54fbc3c13f8bd5b4d873328
tringapps-Mariappan <123533770+Mariviknesh@users.noreply.github.com>
2023-03-10 18:32:11 +0530

    initial commit

---
+++ ImageGallery.js
@@ -0,0 +1,8 @@
+let on=(srcs)=>{
+    document.getElementById("display").src=srcs;
+    document.getElementById("image").style.display="flex";
+}
+let off=(src)=>
+{
+    document.getElementById("image").style.display="none";
+}
